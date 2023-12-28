e6d7791a6e3e750b56a36102ae2b44041987bc5c
Muhammadhussain1999 <ksaad7933@gmail.com>
2023-12-28 17:38:44 +0500

    Added code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,20 @@ import "./App.css";
 import Header from "./components/Header"
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
-import logo from './assests/images/logo192.png'
+// import logo from './assests/images/logo192.png'
 
 
 function App() {
   // const style={backgroundColor:"Blue", fontSize:40}
+  const logo= require('./assests/images/logo192.png')
   const myStyle="red"
   return (
     <>
-      <Header />
+      <Header reactlogo={logo} logoName="react logo"/>
       <h1 className="red">HomePage</h1>
       <p className={myStyle}>This is the homepage</p>
       {/*<img src={logo} alt="logo"/>*/}
-      <img src={require('./assests/images/logo192.png')} alt="logo"/>
+      {/*<img src={require('./assests/images/logo192.png')} alt="logo"/>*/}
 
       <Footer/>
     </>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
-export default function Header() {
+export default function Header(props) {
     return (
       <nav className="nav-bar ">
-        <p>Logo</p>
+        <p><img src={props.reactlogo} alt={props.logoName} height="40"/></p>
         <ul>
           <li>
             <a href="/">Home</a>
